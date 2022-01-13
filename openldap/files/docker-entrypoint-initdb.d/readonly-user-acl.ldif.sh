9b952607cba5c6e76b2b39092c3b04c104ffcf16
Erwin Müller <erwin.mueller@deventm.org>
2022-01-13 04:51:50 +0100

    Add readonly user

--- openldap/files/docker-entrypoint-initdb.d/readonly-user-acl.ldif.sh
+++ openldap/files/docker-entrypoint-initdb.d/readonly-user-acl.ldif.sh
@@ -1,1 +1,18 @@
- 
+#!/bin/bash
+
+. /opt/bitnami/scripts/libvalidations.sh
+
+if ! is_boolean_yes "$LDAP_READONLY_USER"; then
+  exit 0
+fi
+
+cat << "EOF"
+dn: olcDatabase={2}${LDAP_BACKEND},cn=config
+changetype: modify
+delete: olcAccess
+-
+add: olcAccess
+olcAccess: to * by dn.exact=gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth manage by * break
+olcAccess: to attrs=userPassword,shadowLastChange by self write by dn="cn=admin,${LDAP_ROOT_DN}" write by anonymous auth by * none
+olcAccess: to * by self read by dn="cn=admin,${LDAP_ROOT_DN}" write by dn="cn=${LDAP_READONLY_USER_USERNAME},${LDAP_ROOT_DN}" read by * none
+EOF
--- openldap/files/docker-entrypoint-initdb.d/readonly-user.ldif.sh
+++ openldap/files/docker-entrypoint-initdb.d/readonly-user.ldif.sh
@@ -1,1 +1,21 @@
- 
+#!/bin/bash
+
+. /opt/bitnami/scripts/libvalidations.sh
+
+if ! is_boolean_yes "$LDAP_READONLY_USER"; then
+  exit 0
+fi
+
+LDAP_READONLY_USER_PASSWORD_ENCRYPTED="$(echo -n $LDAP_READONLY_USER_PASSWORD |
+ slappasswd -n -T /dev/stdin)"
+
+cat << "EOF"
+# Paths
+dn: cn=${LDAP_READONLY_USER_USERNAME},${LDAP_ROOT_DN}
+changetype: add
+cn: ${LDAP_READONLY_USER_USERNAME}
+objectClass: simpleSecurityObject
+objectClass: organizationalRole
+userPassword: ${LDAP_READONLY_USER_PASSWORD_ENCRYPTED}
+description: LDAP read only user
+EOF
